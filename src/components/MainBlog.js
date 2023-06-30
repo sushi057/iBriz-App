4864d276d4a70c016c9cdeaa9ce8e0f58ac5eb4f
suvash <champsuvash@gmail.com>
2023-06-30 22:46:32 +0545

    test edit feature

--- src/components/MainBlog.js
+++ src/components/MainBlog.js
@@ -5,20 +5,53 @@ import BlogItem from "./BlogItem";
 import articles from "../assets/articles";
 
 function MainBlog() {
+  const [blogList, setBlogList] = useState(articles);
+  const [selectedBlog, setSelectedBlog] = useState(null);
+
+  const handleEdit = (id) => {
+    const selectedBlogItem = blogList.find((blog) => blog.id === id);
+    setSelectedBlog(selectedBlogItem);
+  };
+
+  const handleDelete = (id) => {
+    const updatedBlogList = blogList.filter((blog) => blog.id !== id);
+    setBlogList(updatedBlogList);
+  };
+
+  const handleUpdateBlog = (updatedBlog) => {
+    const updatedBlogList = blogList.map((blog) => {
+      if (blog.id === updatedBlog.id) {
+        return updatedBlog;
+      }
+      return blog;
+    });
+    setBlogList(updatedBlogList);
+    setSelectedBlog(null);
+  };
+
   return (
     <div className="main-blog">
       <div className="blog-list">
-        {articles.map((blog) => (
+        {blogList.map((blog) => (
           <BlogItem
             key={blog.id}
+            id={blog.id}
             title={blog.title}
             author={blog.author}
             date={blog.date}
             content={blog.content}
-            onClick={() => {}}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
           />
         ))}
       </div>
+      {selectedBlog && (
+        <div className="edit-blog">
+          <h2>Edit Blog</h2>
+          {/* Render form or component for editing the blog */}
+          {/* Pass selectedBlog and handleUpdateBlog as props */}
+        </div>
+      )}
       <div className="illustration">
         <img src={illustration} alt="man reading book" />
       </div>
