847e939c204571db0f172caaa1ea9861bee57d4b
suvash <champsuvash@gmail.com>
2023-06-30 22:57:00 +0545

    edit, delete feature

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -1,35 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Blogitem.css";
 import editBtn from "../assets/images/edit.svg";
 import deleteBtn from "../assets/images/delete.svg";
 
 function BlogItem({ id, title, author, date, content, onEdit, onDelete }) {
-  function handleEdit() {
-    onEdit(id);
-  }
-  function handleDelete() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(title);
+  const [editedContent, setEditedContent] = useState(content);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    // Save the edited blog with updated title and content
+    const updatedBlog = {
+      id,
+      title: editedTitle,
+      author,
+      date,
+      content: editedContent,
+    };
+    setIsEditing(false);
+    onEdit(updatedBlog);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setEditedTitle(title);
+    setEditedContent(content);
+  };
+
+  const handleDelete = () => {
     onDelete(id);
-  }
-  
+  };
+
   return (
     <div className="blog-item">
       <div className="blog-header">
         <div className="heading">
-          <h2 className="blog-title">{title}</h2>
-          <div className="heading-icons">
-            <img
-              src={editBtn}
-              className="blog-icons"
-              alt="edit button"
-              onClick={handleEdit}
-            />
-            <img
-              src={deleteBtn}
-              className="blog-icons"
-              alt="delete button"
-              onClick={handleDelete}
-            />
-          </div>
+          {isEditing ? (
+            <>
+              <input
+                type="text"
+                value={editedTitle}
+                onChange={(e) => setEditedTitle(e.target.value)}
+              />
+              <textarea
+                value={editedContent}
+                onChange={(e) => setEditedContent(e.target.value)}
+              ></textarea>
+            </>
+          ) : (
+            <>
+              <h2 className="blog-title">{title}</h2>
+              <div className="heading-icons">
+                <img
+                  src={editBtn}
+                  className="blog-icons"
+                  alt="edit button"
+                  onClick={handleEdit}
+                />
+                <img
+                  src={deleteBtn}
+                  className="blog-icons"
+                  alt="delete button"
+                  onClick={handleDelete}
+                />
+              </div>
+            </>
+          )}
         </div>
         <div className="blog-detail">
           <p className="blog-author">{author}</p>
@@ -37,6 +77,12 @@ function BlogItem({ id, title, author, date, content, onEdit, onDelete }) {
         </div>
       </div>
       <p className="blog-content">{content}</p>
+      {isEditing && (
+        <div className="edit-buttons">
+          <button onClick={handleSave}>Save</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </div>
+      )}
     </div>
   );
 }
